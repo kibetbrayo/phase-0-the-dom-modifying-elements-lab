16e132a52f89d0ae622ac63e1e873573fc35f25e
iam <brian.kibet2@student.moringaschool.com>
2023-06-22 11:07:30 +0000

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Create a new h1 element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute of the new h1 element to 'victory'
+newHeader.id = 'victory';
+
+// Update the text content of the 'h1' element
+newHeader.textContent = 'BRIAN-BRIAN is the champion';
+
+// Append the new h1 element to the body of the document
+document.body.appendChild(newHeader);
